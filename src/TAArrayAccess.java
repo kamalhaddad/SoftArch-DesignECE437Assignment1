720d1ebab6d19a91e4ab8e38db5f4b36156c293b
Kamal Haddad <kdawg4.956@gmail.com>
2017-02-12 21:59:54 +0200

    Added constructors for TAInt

--- src/TAArrayAccess.java
+++ src/TAArrayAccess.java
@@ -12,6 +12,14 @@ public class TAArrayAccess<T extends TADataType>  implements TADataType{
         element = a.array.get((int)c.getValue());
     }
 
+    TAArrayAccess(TAArray<T> a, TAInt i){
+        element = a.array.get((int)i.getValue());
+    }
+
+    TAArrayAccess(String s, TAArray<T> a, TAInt i) throws Exception {
+        NamesChecker.check(s);
+        element = a.array.get((int)i.getValue());
+    }
     @Override
     public void list() {
 
